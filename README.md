aef90dfe13cf59a57ef92b88ffa8c4233029f0da
x <57688724+AeronRoemer@users.noreply.github.com>
2021-07-26 14:04:33 -0600

    Switch to Xpath and WebDriver Wait Working

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Daily Steps:
 1. [X] Format Name List
 2. [X] Selenium Form Entry
 3. Handle Data from Single Name
-    a. Sort Page Source Data 
-    b. [X] Exception Handling
-    c. [X] Upgrade - Wait until Loaded
+    * Sort Page Source Data 
+    * [X] Exception Handling
+    * [X] Upgrade - Wait until Loaded
 4. Loop Handle Data
--- main.py
+++ main.py
@@ -8,6 +8,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 # binary file needed for using browser
 DRIVER_PATH = './driver/chromedriver'
@@ -24,9 +26,9 @@ driver.get("https://a073-ils-web.nyc.gov/inmatelookup/pages/home/home.jsf")
 current_name= "Smith"
 try:
     # wait until current name form is present to add name and submit via click
-    WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.ID, "home_form:j_id_25")))
-    driver.find_element_by_id("home_form:j_id_25").send_keys(current_name)
-    driver.find_element_by_id("home_form:search_btn").click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='home_form:search_btn']")))
+    driver.find_element_by_xpath("//*[@id='home_form:j_id_25']").send_keys(current_name)
+    driver.find_element_by_xpath("//*[@id='home_form:search_btn']").click()
     print(driver.page_source)
     driver.quit()
 except Exception as e: 
