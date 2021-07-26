856587ec2c9efd41e4cb27b9cba9861e439483ee
x <57688724+AeronRoemer@users.noreply.github.com>
2021-07-26 14:19:53 -0600

    Pulling Rows/Cols Data

--- main.py
+++ main.py
@@ -22,14 +22,17 @@ options.add_argument("--window-size=1920,1200")
 # creates webdriver
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 driver.get("https://a073-ils-web.nyc.gov/inmatelookup/pages/home/home.jsf")
-
+xpaths = "/html/body/div[1]/div/div/form/div[3]/div/table/tbody/tr[2]/td[1]"
 current_name= "Smith"
 try:
     # wait until current name form is present to add name and submit via click
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='home_form:search_btn']")))
     driver.find_element_by_xpath("//*[@id='home_form:j_id_25']").send_keys(current_name)
     driver.find_element_by_xpath("//*[@id='home_form:search_btn']").click()
-    print(driver.page_source)
+    rows = 1 +len(driver.find_elements_by_xpath("/html/body/div[1]/div/div/form/div[3]/div/table/tbody/tr"))
+    cols = len(driver.find_elements_by_xpath("/html/body/div[1]/div/div/form/div[3]/div/table/tbody/tr[1]/td"))
+    print(rows, cols)
     driver.quit()
 except Exception as e: 
     print(e)
+
