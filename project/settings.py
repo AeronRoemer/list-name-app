086ba322aa9de53c7cd2746a531f8a9c223db082
x <57688724+AeronRoemer@users.noreply.github.com>
2021-08-11 11:36:32 -0600

    Typo Fixed

--- project/settings.py
+++ project/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'all_names',
         'USER': 'myprojectuser',
-        'PASSWORD': 'passoword',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '',
     }
