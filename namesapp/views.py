74bf6b0ef7f1d205fa1fbf471e2b0abf597ecba4
x <57688724+AeronRoemer@users.noreply.github.com>
2021-08-27 20:49:29 -0600

    Debug update 7

--- namesapp/views.py
+++ namesapp/views.py
@@ -30,6 +30,8 @@ options = Options()
 options.headless = True
 options.add_argument('--no-sandbox')
 options.add_argument('--headless')
+options.add_argument('--verbose')
+options.add_argument('--disable-gpu')
 options.add_argument('--disable-extensions')
 
 
